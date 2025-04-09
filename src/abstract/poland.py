732ae37dd661a76d50a241343e3e18870e1b5000
快乐的老鼠宝宝 <laoshubaby@protonmail.com>
2025-04-09 17:11:32 +0900

    feat: design how to abstract for different api

---
+++ src/abstract/poland.py
@@ -0,0 +1,2 @@
+# this is complaince layer for the library written by a polish developer
+# sorry I forgot the name of package
---
+++ src/abstract/yuheng.py
@@ -0,0 +1,20 @@
+# this layer will pass operation to yuheng default library directly.
+
+def changeset_close():
+    pass
+def changeset_create():
+    pass
+def changeset_update():
+    pass
+def changeset_upload():
+    pass
+def element_create():
+    pass
+def element_delete():
+    pass
+def element_read():
+    pass
+def element_update():
+    pass
+def oauth_login():
+    pass
